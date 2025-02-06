4e29e3eddcdc000b51969eb26eb4e8dbcb258ecd
Jacob Whitney <jacob@jacobdellprec.>
2025-02-06 17:32:58 -0500

    Add contents of Character class and test it in Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Test CharacterList
+
+
+
+        // Test Character
+        /*
+        Character c1 = new Character(
+                1,
+                "Jim Henson",
+                "Rogue",
+                "Gnome",
+                12,
+                13,
+                14
+        );
+        
+        System.out.println(c1.getId());
+        System.out.println(c1.getName());
+        System.out.println(c1.getClassification());
+        System.out.println(c1.getStr());
+        System.out.println(c1.getDex());
+        System.out.println(c1.getCon());
+
+        c1.setId(2);
+        c1.setName("John Smith");
+        c1.setClassification("Dwarf");
+        c1.setStr(19);
+        c1.setDex(18);
+        c1.setCon(17);
+
+        System.out.println();
+        System.out.println(c1.getId());
+        System.out.println(c1.getName());
+        System.out.println(c1.getClassification());
+        System.out.println(c1.getStr());
+        System.out.println(c1.getDex());
+        System.out.println(c1.getCon());
+         */
     }
 }
